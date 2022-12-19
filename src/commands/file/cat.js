d4ca3e0779130a95155aa4e0bc296e37c6af2c37
sa58 <likhacheva.392@gamil.com>
2022-12-19 16:39:05 +0300

    fix: fix cat error throw

--- src/commands/file/cat.js
+++ src/commands/file/cat.js
@@ -1,6 +1,7 @@
 import { createReadStream } from 'fs';
 import { access, stat } from 'fs/promises';
 import { resolve } from 'path';
+import { OpearationFailedError } from '../../error/opearation-failed-error.js';
 import { getCWD } from '../../lib/get-cwd.js';
 import { printCWD } from '../../lib/print-cwd.js';
 
