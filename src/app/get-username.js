d08b71bfeb614e47b61ff2edc9278c902eae356c
sa58 <likhacheva.392@gamil.com>
2022-12-19 22:14:26 +0300

    refactor: clear try catch

--- src/app/get-username.js
+++ src/app/get-username.js
@@ -6,23 +6,17 @@ const USER_ARGUMENT = '--username';
 const SEPARATOR = '=';
 
 const getUsername = () => {
-    try {
-        const cliArguments = argv.slice(2);
-        const [nameArgument] = cliArguments.filter(arg => arg.startsWith(USER_ARGUMENT))
+    const cliArguments = argv.slice(2);
+    const [nameArgument] = cliArguments.filter(arg => arg.startsWith(USER_ARGUMENT))
 
-        const [_, name] = nameArgument.split(SEPARATOR);
-        // console.log(name)
-        
-        // if (!name) {
-        //     throw new NotAuthorizedException();
-        // }
+    const [_, name] = nameArgument.split(SEPARATOR);
+    // console.log(name)
+    
+    // if (!name) {
+    //     throw new NotAuthorizedException();
+    // }
 
-        return name;
-    } catch (exc) {
-        throw exc;
-    }
-
-
+    return name;
 
     // console.log('anon')
     // return 'Anonymous';
